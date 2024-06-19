430d9ba24e298b8e697ec49a8656132c40d7bad1
Rohan <techhack237@gmail.com>
2024-06-19 18:13:32 +0530

    first commit

---
+++ src/app/data.js
@@ -0,0 +1,96 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Hotel Booking app",
+    description: "Book your hotels safely, secure and fast",
+    date: "2024-04-15",
+    demoLink: "https://mern-booking-app-21ql.onrender.com/",
+  },
+  {
+    id: 2,
+    name: "Block Game",
+    description: "Small game by react",
+    date: "2023-08-20",
+    demoLink: "https://react-asgn-priyathik.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "MyCryptoUpdate",
+    description: "All updates by latest crypto",
+    date: "2023-05-10",
+    demoLink: "https://react-crypto-app-murex-delta.vercel.app/",
+  },
+  {
+    id: 4,
+    name: "Landing Page",
+    description: "Beautiful landing page by css",
+    date: "2022-05-30",
+    demoLink: "https://landingpage-xi-two.vercel.app/",
+  },
+  {
+    id: 5,
+    name: "Newsapp",
+    description: "Check latest indian & globalnews with multiple types",
+    date: "2022-06-12",
+    demoLink: "https://github.com/PriyathikGit/Newsapp/",
+  },
+  {
+    id: 6,
+    name: "Google Chat",
+    description: "Small google chat clone implemented everything in js",
+    date: "2023-12-21",
+    demoLink: "https://6672cece83621c15d9373ae7--neon-arithmetic-1c8375.netlify.app/",
+  },
+  
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://www.github.com/priyathikgit",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/priyathik-raj",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "X",
+    link: "https://x.com/Priyathikraj",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
